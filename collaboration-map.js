bbea1fc1ab2f6d2edbeefc85ac1050ad1712f445
saif429 <saif429@gmail.com>
2017-11-20 09:00:16 -0500

    trying to get it to work again

--- collaboration-map.js
+++ collaboration-map.js
@@ -399,17 +399,12 @@ var CollaborationMap = function(chartElementId, infoElementId, dataJson) {
         var ac = d.selectAll(".detail").data(Z, u);
         var Y = ac.enter().append("g").attr("class", "detail");
         var ab = Z[0];
-        //if (ab && ab.type === "ditem") {
-        //    var aa = Y.append("a").attr("xlink:href", function(ae) {
-        //        return "/" + ae.slug
-        //    });
-
-        // **Should remove the link possibly from the circle! Testing! **
-
-
+        if (ab && ab.type === "ditem") {
+            var aa = Y.append("a").attr("xlink:href", function(ae) {
+                return "/" + ae.slug
+            });
             aa.append("text").attr("fill", N).attr("text-anchor", "middle").attr("y", (o + t) * -1).text(function(ae) {
-                // **Should remove the ITEM text in the circle in product page, however link still remains. Find where to remove link!
-                // return "ITEM " + ae.ditem
+                // **Should remove the ITEM text in the circle in product page** return "ITEM " + ae.ditem
             })
         } else {
             if (ab && ab.type === "product") {
